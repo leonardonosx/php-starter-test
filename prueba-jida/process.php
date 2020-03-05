eb11981427d56da3dc266aaa2d52258cfa4cff88
leonardo <leonardonosx@gmail.com>
2020-03-05 18:50:49 -0400

    Validacion de usuario y consulta de post

--- prueba-jida/process.php
+++ prueba-jida/process.php
@@ -1,5 +1,6 @@
 <?php
 	//Cabeceras
+
 	header("Access-Control-Allow-Origin: *");
 	header("Content-Type: application/json; charset=UTF-8");
 	header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
@@ -18,10 +19,47 @@
  	$array = json_decode($resultados, true);
 
 	foreach ($array as $row) {
-	    $arrConnections = $row["status"];
+	    if(is_array($row)){
+			foreach ($row as $status) {
+				$arrConnections = $row["status"];
+				if($arrConnections == "connected"){
+		    		//Usuario Valido - iniciar session
+		    		session_start();
+		    		//echo session_id();
+		    		
+
+					$post ='https://api.dev.graphs.social/v4/graphs?containers_ids=5d0051fc3039353ff68410e8&entities_ids=14%2C23%2C48%2C49%2C50%2C51&limit=30&next=time_updated';
+
+					$publicaciones = file_get_contents($post);
+					$post_array = json_decode($publicaciones, true);
+					//print_r($post_array);
+
+						foreach ($post_array as $p_array ) {
+							if(is_array($p_array)){
+								foreach($p_array as $key) {
+							        if(is_array($key)){
+							        	foreach($key as $document){
+							        		//print_r($document);
+							        		if(is_array($document)){
+								        		foreach($document as $docu){
+
+								        			print_r($docu);
+								        			
+								        		}
+								            }	
+							        	}
+							        }
+
+							    }
+							}    
+
+						
+			    } else {
+			    	//echo "Usuario Invalido";
+			    	header("Location: http://localhost/prueba-jida");
+					exit;
+			    }
+			}
+		}
 	}
-
-	echo ($arrConnections);
-	//print_r($arrConnections) ;
-
 ?>
